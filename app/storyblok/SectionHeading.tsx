77b774249d59a52c6f5d824a0a709a7a6625fdeb
orvvro <81515271+orvvro@users.noreply.github.com>
2025-08-06 22:34:17 +0200

    Update SectionHeading.tsx

--- app/storyblok/SectionHeading.tsx
+++ app/storyblok/SectionHeading.tsx
@@ -1,5 +1,6 @@
 import { storyblokEditable, type SbBlokData } from "@storyblok/react";
-
+import { formatText } from "~/lib/formatText";
+import { css, cx } from "@linaria/core";
 export default function SectionHeading({
   blok,
 }: {
@@ -7,12 +8,35 @@ export default function SectionHeading({
     // Add any custom fields from your Storyblok component here.
     heading?: string;
     sub_heading?: string;
+    style: string;
   };
 }) {
   return (
-    <div {...storyblokEditable(blok)}>
-      {blok.heading && <h1>{blok.heading}</h1>}
+    <div
+      {...storyblokEditable(blok)}
+      className={cx(headingContainer, blok.style)}
+    >
+      {blok.heading && <h1>{formatText(blok.heading)}</h1>}
       {blok.sub_heading && <p>{blok.sub_heading}</p>}
     </div>
   );
 }
+
+const headingContainer = css`
+  h1 {
+    margin-bottom: 1em;
+  }
+
+  h1:not(:last-child) {
+    margin-bottom: 0.5em;
+  }
+
+  p {
+    font-size: 1.3em;
+    margin-bottom: 2em;
+  }
+
+  &.center {
+    text-align: center;
+  }
+`;
